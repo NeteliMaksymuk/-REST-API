3252f15d6a7fe9d80c5aed1f4131aef3201f2bb6
Neteli Maksymuk <maksymuknatalia@gmail.com>
2023-01-02 14:15:35 +0200

    Add task in README.md and import all reauirements for auth

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,19 @@
+from flask_jwt_extended import JWTManager
+from flask_smorest import Api
+import os
+
 
 from flask import Flask
+
+from project.db import db
 
 app = Flask(__name__)
 
 import project.views
+
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+db.init_app(app)
+
+api = Api(app)
+
+jwt = JWTManager(app)
